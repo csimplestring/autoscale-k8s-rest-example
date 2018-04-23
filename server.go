7f957326e90434679c7899d3199d8744d07bf4af
Yi Wang <wang.yi@yieldr.com>
2018-04-23 12:10:02 +0200

    fix old path;

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/csimplestring/echo-rest-example/customer"
+	"github.com/csimplestring/autoscale-k8s-rest-example/customer"
 	"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 	"github.com/labstack/gommon/log"
